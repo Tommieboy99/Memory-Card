e5693abc2ac251dff73dc48090e94a476318c311
Tommieboy99 <tomkoek99@hotmail.com>
2025-12-28 12:22:21 +0100

    Eerste poging

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <>
+    <Header/>
+    <MemoryGame/>
+    </>
+  )
+}
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='intro'>
+        <h1>
+          Pokemon Memory Card
+        </h1>
+        <p>
+          Klik op alle afbeeldingen 1 keer en win de game. 
+        </p>
+      </div>
+    </header>
+  )
+}
+
+function MemoryGame() {
+  const [clickedCards, setClickedCards] = useState([]);
+  const [count, setCount] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [cards, setCards] = useState(['pikachu', 'bulbasaur', 'charmander', 'snorlax', 'pidgeot', 'caterpie', 'igglybuff', 'onix', 'umbreon', 'seadra', 'wooper', 'tentacool'])
+
+  function handleClick( name ) {
+
+    if (!clickedCards.includes(name)) {
+      setClickedCards(prev => [...prev, name])
+      setCount(count => count + 1)
+    } else {
+
+      if (count > highScore) {
+        setHighScore(count)
+      }
+
+      setCount(0)
+      setClickedCards([])
+    }
+
+    setCards(prev => {
+      const result = [...prev];
+
+      for (let i = result.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [result[i], result[j]] = [result[j], result[i]];
+      }
+
+      return result;
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <p>Score: {count}</p>
+      <p>Highscore: {highScore}</p>
+    </div>
+    <div className='cardContainer'>
+      {cards.map(card => <MemoryCard key={card} name={card} handleClick={() => handleClick(card)} />)}
+    </div>
     </>
+  )
+}
+
+function MemoryCard ( {name, handleClick} ) {
+  const [imgUrl, setImgUrl] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(response => response.json())
+      .then(data => {
+        setImgUrl(data.sprites.other.dream_world.front_default)
+      })
+      .catch(console.error)
+  }, [name]);
+
+  return (
+    <button onClick={handleClick}>
+      <img className='pokemonImage' src={imgUrl} alt="" />
+    </button>
   )
 }
 
